064e35bbf4f926d3685b18bea0fd580f9be81ecb
fr13nd <fr13nd65@protonmail.com>
2024-07-31 23:15:39 +0200

    remove ai

--- README.md
+++ README.md
@@ -6,11 +6,3 @@ DISCORD_TOKEN="<replace with your own bot token>"
 ```
 2. Run the bot:
 `cargo run --release`
-
-
-## TODO:
-- [ ] add error handling for "random" crashes in generate_ai_response() function
-- [ ] send some fail message so that the user will know that it failed to generate response (currently if fails, no response)
-- [ ] make it faster somehow
-- [ ] make it remember the chat history (use api/chat instead of api/generate to generate responses)
-- [ ] make it work if it gets **any** ping or user replies to the bot's message
--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,13 @@ use dotenv::dotenv;
 
 use serenity::all::{ChannelId, Member};
 use serenity::async_trait;
-use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
 struct Handler;
 
-async fn generate_ai_response(ctx: Context, msg: Message) {
-    let output = std::process::Command::new("curl")
-        .arg("http://localhost:11434/api/generate")
-        .arg("-d")
-        .arg(format!(
-            r#"{{"model": "gemma:2b", "prompt": "{}"}}"#,
-            msg.content.replace("\n", "\\n")
-        ))
-        .output()
-        .expect("Failed to execute command");
-
-    let output_str = String::from_utf8_lossy(&output.stdout);
-    let lines: Vec<_> = output_str.lines().collect();
-
-    let mut sentence = String::new();
-
-    for line in lines {
-        let json: serde_json::Value = serde_json::from_str(line).unwrap();
-        let response = json["response"].as_str().unwrap();
-        sentence.push_str(response);
-        sentence = sentence
-            .replace("@everyone", "`<everyone ping>`")
-            .replace("<@&1143759057778393200>", "`<member role ping>`")
-            .replace("@member", "`<member role ping>`")
-            .replace("@here", "`<here ping>`");
-
-        if let Some(done) = json.get("done") {
-            if done.as_bool().unwrap() == true {
-                break;
-            }
-        }
-    }
-
-    sentence.pop();
-
-    msg.reply(ctx.clone(), sentence).await.unwrap();
-}
-
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content.starts_with("!ai") || msg.content.contains("<@1143408254127448104>") {
-            let _ = msg
-                .content
-                .strip_prefix("!ai")
-                .unwrap_or_default()
-                .replace("\n", "\\n")
-                .replace("<@1143408254127448104>", "");
-            generate_ai_response(ctx, msg).await;
-        }
-    }
-
     async fn guild_member_addition(&self, ctx: Context, new_member: Member) {
         let channel_id = ChannelId::new(1143610931708965019);
         let welcome_message = format!("Welcome {}", new_member.mention());
